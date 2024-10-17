f8c8676a6240fc470440334976b536eed024f508
AndrewChebli <andrew.chebli@outlook.com>
2024-10-17 01:59:23 -0400

    loading the data to naive_bayes.py

--- models/naive_bayes.py
+++ models/naive_bayes.py
@@ -1,16 +1,12 @@
 import torch
+import numpy as np
 from torchvision.models import resnet18, ResNet18_Weights
 import torch.nn as nn
 import torchvision.transforms as transforms
+import resnet18_model
 
-def get_resnet():
-    #load pre trained model resnet 18
-    resnet18_model = resnet18(weights= ResNet18_Weights.IMAGENET1K_V1)
+#load the features and labels
+def load_labels_features():
+    train_features = torch.load('data/extracted_data/features.pt')
+    train_labels = torch.load('data/extracted_data/labels.pt')
 
-    #remove last layer to only use feature extraction from the model
-    newmodel = nn.Sequential(*(list(resnet18_model.children())[:-1]))
-    return newmodel
-
-if __name__ == "__main__":
-    resnet_18 = get_resnet()
-    print (resnet_18)
