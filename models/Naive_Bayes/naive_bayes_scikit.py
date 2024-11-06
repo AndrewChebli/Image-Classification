c75389a8f004f636e161316e746dcb58bc619ced
AndrewChebli <andrew.chebli@outlook.com>
2024-11-05 20:06:17 -0500

    create a class for skicitNaiveBayesModel

--- models/Naive_Bayes/naive_bayes_scikit.py
+++ models/Naive_Bayes/naive_bayes_scikit.py
@@ -3,27 +3,49 @@ from sklearn.metrics import accuracy_score
 import torch
 import numpy
 
+class ScikitNaiveBayesModel():
+    def __init__(self):
+        self.model = GaussianNB()
 
-def load_train_labels_features():
-    data = torch.load('data/extracted_data/train_data.pt', weights_only=True)
-    train_features = data['features'].numpy()
-    train_labels = data['labels'].numpy()
+    def load_train_labels_features(self):
+        data = torch.load('data/extracted_data/train_data.pt', weights_only=True)
+        train_features = data['features'].numpy()
+        train_labels = data['labels'].numpy()
 
-    return train_features, train_labels
+        return train_features, train_labels
 
-def load_test_labels_features():
-    data = torch.load('data/extracted_data/test_data.pt', weights_only=True)
-    test_features = data['features'].numpy()
-    test_labels = data['labels'].numpy()
+    def load_test_labels_features(self):
+        data = torch.load('data/extracted_data/test_data.pt', weights_only=True)
+        test_features = data['features'].numpy()
+        test_labels = data['labels'].numpy()
 
-    return test_features, test_labels
+        return test_features, test_labels
 
-# Load train and test data
-train_features, train_labels = load_train_labels_features()
-test_features, test_labels = load_test_labels_features()
+    def train(self, train_features, train_labels):
+        # train using the Scikit-Learn's GaussianNB
+        self.model.fit(train_features, train_labels)
 
-# Train and test using the Scikit-Learn's GaussianNB
-gnb = GaussianNB()
-y_prediction = gnb.fit(train_features, train_labels).predict(test_features)
-scikit_accuracy = accuracy_score(y_prediction, test_labels)
-print(f"Scikit-Learn GaussianNB Accuracy: {scikit_accuracy * 100:.2f}%")
+    def predict(self, test_features):
+        # predict using the trained model
+        return self.model.predict(test_features)
+
+    def evaluate_model(self, y_prediction, test_labels):
+        return accuracy_score(y_prediction, test_labels) * 100
+
+if __name__ == "__main__":
+
+    skicit_model = ScikitNaiveBayesModel()
+
+    # Load the saved training data and testing data
+    train_features, train_labels = skicit_model.load_train_labels_features()
+    test_features, test_labels = skicit_model.load_test_labels_features()
+
+    # Train the model with the data we have
+    skicit_model.train(train_features, train_labels)
+
+    # Predict on the test data
+    y_prediction = skicit_model.predict(test_features)
+
+    # Evaluate accuracy
+    scikit_accuracy = skicit_model.evaluate_model(y_prediction, test_labels)
+    print(f"Scikit-Learn GaussianNB Accuracy: {scikit_accuracy:.2f}%")
