d151d73a841186728255ddea59c56975c5cdc898
AndrewChebli <andrew.chebli@outlook.com>
2024-11-17 23:00:10 -0500

    main function to train, validate, and save best model

--- models/CNN/cnn.py
+++ models/CNN/cnn.py
@@ -126,3 +126,70 @@ class CNN(nn.Module):
         #pass through the fully connected layers, classifier
         x = self.classifier(x)
         return x
+
+if __name__ == '__main__':
+
+    batch_size = 32
+    test_batch_size = 32
+    input_size = 3 * 32 * 32  # 3 channels, 32x32 image size
+    # hidden_size = 50  # Number of hidden units
+    output_size = 10  # Number of output classes (CIFAR-10 has 10 classes)
+    num_epochs = 50
+    all_models=[]
+
+
+    train_dataset, test_loader = cifar_loader(batch_size)
+    _, test_loader = cifar_loader(test_batch_size)
+
+    device = torch.device("mps" if torch.backends.mps.is_available() else "cuda:0" if torch.cuda.is_available() else "cpu")
+    model = CNN(input_size, 10, output_size)
+    model = nn.DataParallel(model)
+    model.to(device)
+    #model.load_state_dict(torch.load('path'))
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr = 0.001, momentum=0.9)
+    BestACC=0
+    
+    # Split the training dataset into 90% training and 10% evaluation
+    train_size = int(0.9 * len(train_dataset))
+    eval_size = len(train_dataset) - train_size
+    train_subset, eval_subset = torch.utils.data.random_split(train_dataset, [train_size, eval_size])
+
+
+    
+    for epoch in range(num_epochs):
+        train_loader = td.DataLoader(train_subset, batch_size=batch_size, shuffle=True, pin_memory=True)
+        eval_loader = td.DataLoader(eval_subset, batch_size=batch_size, shuffle=False, pin_memory=True)
+
+        running_loss = 0
+        for instances, labels in train_loader:
+            optimizer.zero_grad()
+
+            output = model(instances.to(device))
+            loss = criterion(output, labels.to(device))
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        model.eval()
+        with torch.no_grad():
+            allsamps=0
+            rightPred=0
+
+            for instances, labels in eval_loader:
+                output = model(instances.to(device))
+                predictedClass=torch.max(output,1)
+                allsamps+=output.size(0)
+                rightPred+=(torch.max(output,1)[1]==labels.to(device)).sum()
+
+            ACC=float(rightPred)/float(allsamps)
+            print(f'Evaluation Accuracy is={ACC*100}, Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+            if ACC>BestACC:
+                BestACC=ACC
+                all_models.append(model.state_dict())
+        model.train()
+    print(f'best accuracy for cnn is {BestACC*100}')
+    torch.save(all_models[-1], './output/cnn_model.pth')
+
